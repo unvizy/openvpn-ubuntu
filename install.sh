6059711a35b68f21f0edc1c51e1cbbc6f29907cd
git <unvizy17@gmail.com>
2023-04-20 13:25:42 +0700

    change statement

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ version=`lsb_release -cs`
 if [ $version == 'xenial' ]
 then
     dpkg -i openvpn-version/openvpn_xenial.deb
-else
+
+elif [ $version == 'trusty' ]
+then
     dpkg -i openvpn-version/openvpn_trusty.deb
+else
+    echo "Server using Ubuntu version $version"
 fi
